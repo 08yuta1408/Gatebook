57d27c95078a44423564071d041cb5928778d0f0
Yuta Naito <t415036@st.pu-toyama.ac.jp>
2017-02-04 14:59:33 +0900

    Noteコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get '/notes/new' => 'notes#new'
 
-  get 'home/about'
+  root 'home#top'
+  get '/about' => 'home#about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
