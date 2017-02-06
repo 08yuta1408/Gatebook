d6aa3315dd29c6f87616b0648a8f0e327382fa28
Yuta Naito <t415036@st.pu-toyama.ac.jp>
2017-02-06 14:22:37 +0900

    編集ページで，データの編集を行えるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/notes' => 'notes#index'
   get '/notes/:id' => 'notes#show', as: 'note'
   get '/notes/:id/edit' => 'notes#edit', as: 'note_edit'
+  patch '/notes/:id' => 'notes#update', as: 'note_update'
 
   root 'home#top'
   get '/about' => 'home#about'
