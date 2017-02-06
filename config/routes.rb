e138f93a763f2161274b90c22b586d4322acead7
Yuta Naito <t415036@st.pu-toyama.ac.jp>
2017-02-06 14:04:24 +0900

    投稿の編集ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/notes' => 'notes#create'
   get '/notes' => 'notes#index'
   get '/notes/:id' => 'notes#show', as: 'note'
+  get '/notes/:id/edit' => 'notes#edit', as: 'note_edit'
 
   root 'home#top'
   get '/about' => 'home#about'
